d57cfc68408d1897d1f43bf75440b655eee0adae
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-03-09 17:32:01 +0400

    child process

--- last-child.js
+++ last-child.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// import fetch from "node-fetch";
-
+import fetch from "node-fetch";
+import util from "util";
 const HTTP_PORT = 8039;
 
 main().catch(() => 1);
@@ -9,8 +9,16 @@ main().catch(() => 1);
 // ************************************
 
 async function main() {
-  for (let i = 0; i < 10000000000; i++) {
-    i = i + 1;
+  try {
+    var res = await fetch("http://localhost:8039/get-records");
+    if (res && res.ok) {
+      let records = await res.json();
+      if (records && records.length > 0) {
+        process.exitCode = 0;
+        return;
+      }
+    }
+  } catch (err) {
+    process.exitCode = 1;
   }
-  process.exitCode = 1;
 }
