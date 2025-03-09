5f45760b33a0f8faa87bee5134e06d75f3f8301a
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-03-09 17:54:41 +0400

    spinning processes

--- last.js
+++ last.js
@@ -9,6 +9,7 @@ import childProc from "child_process";
 // ************************************
 
 const HTTP_PORT = 8039;
+const MAX_CHILDREN = 5;
 
 var delay = util.promisify(setTimeout);
 
@@ -18,15 +19,31 @@ main().catch(() => 1);
 
 async function main() {
   console.log(`Load testing http://localhost:${HTTP_PORT}...`);
-  for ()
 
   while (true) {
     process.stdout.write(`Trying ${MAX_CHILDREN} child processes...`);
 
-    var child = childProc.spawn("node", ["last-child.js"]);
-    child.on("exit", function (code) {
-      //  code == 0
+    let children = [];
+    for (let i = 0; i < MAX_CHILDREN; i++) {
+      children.push(childProc.spawn("node", ["last-child.js"]));
+    }
+
+    let resps = children.map(function (child) {
+      return new Promise(function (res) {
+        child.on("exit", function (code) {
+          if (code == 0) res(true);
+          res(false);
+        });
+      });
     });
+
+    resps = await Promise.all(resps);
+
+    if (resps.filter(Boolean).length == MAX_CHILDREN) {
+      console.log("success");
+    } else {
+      console.log("failure");
+    }
 
     console.log("done");
 
